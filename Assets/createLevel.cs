674542083dd827bbe2822c0513dac3ed8a23f014
LOLMEH <lolcrisy@gmail.com>
2025-03-19 15:47:21 -0400

    Update PreAlpha 1

---
+++ Assets/createLevel.cs
@@ -0,0 +1,149 @@
+using System;
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[System.Serializable]
+public class GridPosition
+{
+    public int x;
+    public int y;
+}
+
+[System.Serializable]
+public class ObjectInformation
+{
+    public String type;
+    public GridPosition position;
+}
+
+[System.Serializable]
+public class Level
+{
+    public String levelName;
+    public String bounds;
+    public GridPosition playerStartPosition;
+    public ObjectInformation[] levelData;
+}
+
+[System.Serializable]
+public class LevelBounds
+{
+    public GridPosition[] boundData;
+}
+
+public class createLevel : MonoBehaviour
+{
+    public TextAsset levelFile;
+    public TextAsset oldBoundsFile;
+    public GameObject player;
+    public Tilemap regularBoxTilemap;
+    public TileBase regularBoxTile;
+    public Tilemap steelBoxTilemap;
+    public TileBase steelBoxTile;
+    public Tilemap lavaBoxTilemap;
+    public TileBase lavaBoxTile;
+    public GameObject greenKeyDoor;
+    public GameObject redKeyDoor;
+    public GameObject blueKeyDoor;
+    public GameObject greenKey;
+    public GameObject redKey;
+    public GameObject blueKey;
+    public GameObject powerUp;
+    public GameObject goal;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Get level from json
+        Level levelInfo = JsonUtility.FromJson<Level>(levelFile.text);
+        ObjectInformation[] levelBoxes = levelInfo.levelData;
+
+        // Move player
+        GridPosition playerStartPosition = levelInfo.playerStartPosition;
+        Vector2 playerStartPositionVector = new Vector2(levelInfo.playerStartPosition.x, levelInfo.playerStartPosition.y);
+        player.transform.position = playerStartPositionVector;
+
+        // Get and generate level boundaries
+        if (levelInfo.bounds == "old")
+        {
+            LevelBounds levelBounds = JsonUtility.FromJson<LevelBounds>(oldBoundsFile.text);
+            GridPosition[] levelBoundData = levelBounds.boundData;
+            for (int counter = 0; counter < levelBoundData.Length; counter++)
+            {
+                GridPosition boundPositon = levelBoundData[counter];
+                Vector3Int boundPositonVector = new Vector3Int(boundPositon.x, boundPositon.y);
+                steelBoxTilemap.SetTile(boundPositonVector, steelBoxTile);
+            }
+        }
+        else
+        {
+            print("Error: bounds " + levelInfo.bounds + " not found");
+        }
+
+        // Generate level objects
+        for (int counter = 0; counter < levelBoxes.Length; counter++)
+        {
+            String objectName = levelBoxes[counter].type;
+            GridPosition objectPosition = levelBoxes[counter].position;
+            Vector3Int objectPositonVector = new Vector3Int(objectPosition.x, objectPosition.y);
+
+            if (objectName == "regularBox")
+            {
+                regularBoxTilemap.SetTile(objectPositonVector, regularBoxTile);
+            }
+            else if (objectName == "steelBox")
+            {
+                steelBoxTilemap.SetTile(objectPositonVector, steelBoxTile);
+            }
+            else if (objectName == "lavaBox")
+            {
+                lavaBoxTilemap.SetTile(objectPositonVector, lavaBoxTile);
+            }
+            else if (objectName == "powerUp")
+            {
+                Instantiate(powerUp, objectPositonVector, Quaternion.identity);
+            }
+            else if (objectName == "goal")
+            {
+                Instantiate(goal, objectPositonVector, Quaternion.identity);
+            }
+            else if (objectName == "greenKeyDoor")
+            {
+                Instantiate(greenKeyDoor, objectPositonVector, Quaternion.identity);
+            }
+            else if (objectName == "greenKey")
+            {
+                Instantiate(greenKey, objectPositonVector, Quaternion.identity);
+            }
+            else if (objectName == "redKeyDoor")
+            {
+                Instantiate(redKeyDoor, objectPositonVector, Quaternion.identity);
+            }
+            else if (objectName == "redKey")
+            {
+                Instantiate(redKey, objectPositonVector, Quaternion.identity);
+            }
+            else if (objectName == "blueKeyDoor")
+            {
+                Instantiate(blueKeyDoor, objectPositonVector, Quaternion.identity);
+            }
+            else if (objectName == "blueKey")
+            {
+                Instantiate(blueKey, objectPositonVector, Quaternion.identity);
+            }
+            else
+            {
+                print("Error: object " + objectName + " not found");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/nextLevelScript.cs
+++ Assets/nextLevelScript.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 
 public class nextLevelScript : MonoBehaviour
 {
     public GameObject player;
-    public GameObject gameCamera;
-    public GameObject gameBackground;
-    public CompositeCollider2D regularBoxCollider;
-    public Vector2 newLevelPlayerPositon;
-    public int newLevelCameraX;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,22 +17,11 @@ public class nextLevelScript : MonoBehaviour
     {
         BoxCollider2D playerCollider = player.GetComponent<BoxCollider2D>();
         BoxCollider2D goalCollider = GetComponent<BoxCollider2D>();
-        SpriteRenderer playerPowerUpImage = player.GetComponent<movementScript>().powerUpImage;
-        movementScript playerScript = player.GetComponent<movementScript>();
 
         if (goalCollider.IsTouching(playerCollider))
         {
-            // Update object positions
-            gameCamera.transform.position = new Vector3(newLevelCameraX, 0, -10);
-            gameBackground.transform.position = new Vector3 (newLevelCameraX, 0, 10);
-            player.GetComponent<movementScript>().respawnPoint = newLevelPlayerPositon;
-            player.transform.position = newLevelPlayerPositon;
-            // Remove power up from player
-            playerScript.hasPowerUp = false;
-            playerPowerUpImage.enabled = false;
-            Physics2D.IgnoreCollision(playerCollider, regularBoxCollider, false);
-            // Destroy goal
-            Destroy(gameObject);
+            // Reset scene
+            SceneManager.LoadScene("GameScene");
         }
     }
 }
